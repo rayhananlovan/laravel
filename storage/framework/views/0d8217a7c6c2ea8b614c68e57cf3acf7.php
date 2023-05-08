962ead9b564ed871656fcc7a5a1a9e8c99dae111
Rayhan Nurcholis <rayhan.a.nurcholis@gmail.com>
2023-05-08 22:19:46 +0700

    first commit

---
+++ storage/framework/views/0d8217a7c6c2ea8b614c68e57cf3acf7.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="nav">
+            <div class="logo">
+                <a href="#">
+                    <p>GAS</p>
+                </a>
+            </div>
+            <ul>
+                <li>Beranda</li>
+                <li>Tentang</li>
+                <li>Fitur</li>
+                <li>Daftar</li>
+                <li>Karir</li>
+            </ul>
+        </div>
+        <div class="header">
+            <h1>Bergabung <br>Dengan Gas</h1>
+        </div>
+
+        <div class="content-header">
+            <p>Aplikasi gas akan membantumu memantau stok produk tokomu dan menjangkau customer lebih luas.</p>
+            <br>
+            <input type="text" name="text" class="search-input" placeholder="cari toko...">
+            <input class="search-btn" type="button" value="Cari">
+        </div>
+
+        
+
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\landingpag\resources\views/welcome.blade.php ENDPATH**/ ?>
